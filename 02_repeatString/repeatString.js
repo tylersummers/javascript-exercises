99899b986c16640c276dfdbfd68a67928cb5d901
tylersummers <162672490+tylersummers@users.noreply.github.com>
2025-01-30 16:06:08 +0900

    Complete repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
-};
+const repeatString = function(word, repeatTimes) {
+if (repeatTimes < 0) return "ERROR"
+    let string = ""
+    for (let i = 0; i < repeatTimes; i++) {
+        string += word
+    }
+    return string
+}
 
 // Do not edit below this line
 module.exports = repeatString;
