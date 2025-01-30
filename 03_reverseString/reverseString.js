4abc17a18d914f8be6513595caefed5ed68e77d3
tylersummers <162672490+tylersummers@users.noreply.github.com>
2025-01-30 16:31:53 +0900

    Add for loop to reverse word string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+const reverseString = function(word) {
+if (word.length < 1) return "ERROR"
+let reversedWord = ""
 
-};
+for (let i = word.length - 1; i >= 0; i--) {
+    reversedWord += word.charAt(i);
+}
+return reversedWord
+}
 
 // Do not edit below this line
 module.exports = reverseString;
