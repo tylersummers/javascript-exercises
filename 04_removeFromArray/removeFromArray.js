d58b4fd3cd3eb9787635b903ac8af4d953b6d787
tylersummers <162672490+tylersummers@users.noreply.github.com>
2025-01-31 12:10:16 +0900

    Add function to remove single char

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-};
+
+const removeFromArray = function(array, removedChar) {
+    if (array.includes(removedChar)) {
+        let charIndex = array.indexOf(removedChar);
+        array.splice(charIndex, 1);
+        return array; }
+    }
 
 // Do not edit below this line
 module.exports = removeFromArray;
