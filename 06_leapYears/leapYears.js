b7e10e958495de16621afee53e6668af47fc728d
tylersummers <162672490+tylersummers@users.noreply.github.com>
2025-02-03 16:29:41 +0900

    Add function to check condition for leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,12 @@
-const leapYears = function() {
-
-};
-
+const leapYears = function(year) {
+    if ((Number.isInteger(year / 4) === true) && (Number.isInteger (year / 100) === false)) {
+        return true
+    }   else if ((Number.isInteger(year / 4) === true) && (Number.isInteger (year / 100) === true)) {
+            if (Number.isInteger (year / 400) === true) {
+                return true
+            } else return false
+        }
+        return false
+    }
 // Do not edit below this line
 module.exports = leapYears;
--- 06_leapYears/leapYears.spec.js
+++ 06_leapYears/leapYears.spec.js
@@ -4,19 +4,31 @@ describe('leapYears', () => {
   test('works with non century years', () => {
     expect(leapYears(1996)).toBe(true);
   });
-  test.skip('works with non century years', () => {
+  test('works with non century years', () => {
     expect(leapYears(1997)).toBe(false);
   });
-  test.skip('works with ridiculously futuristic non century years', () => {
+  test('works with ridiculously futuristic non century years', () => {
     expect(leapYears(34992)).toBe(true);
   });
-  test.skip('works with century years', () => {
+  test('works with century years', () => {
     expect(leapYears(1900)).toBe(false);
   });
-  test.skip('works with century years', () => {
+  test('works with century years', () => {
     expect(leapYears(1600)).toBe(true);
   });
-  test.skip('works with century years', () => {
+  test('works with century years', () => {
     expect(leapYears(700)).toBe(false);
   });
 });
+const leapYears = function(year) {
+  if ((Number.isInteger(year / 4) === true) && (Number.isInteger (year / 100) === false)) {
+      return true
+  }   else if ((Number.isInteger(year / 4) === true) && (Number.isInteger (year / 100) === true)) {
+          if (Number.isInteger (year / 400) === true) {
+              return true
+          } else return false
+      }
+      return false
+  }
+// Do not edit below this line
+module.exports = leapYears;
