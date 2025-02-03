519f9b7b79a0cf868517e81aa08ede2b685dc548
tylersummers <162672490+tylersummers@users.noreply.github.com>
2025-02-03 11:54:23 +0900

    Rewrite function to handle complex cases

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,13 @@
 
-const removeFromArray = function(array, removedChar) {
-    if (array.includes(removedChar)) {
-        let charIndex = array.indexOf(removedChar);
-        array.splice(charIndex, 1);
-        return array; }
-    }
+const removeFromArray = function(array, ...args) {
+    const newArray = [];
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray.push(item);
+        }
+    });
+    return newArray;
+};
 
 // Do not edit below this line
 module.exports = removeFromArray;
