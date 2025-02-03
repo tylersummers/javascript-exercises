37b1645748c5e8e31308300112aa52d31bdb4f40
tylersummers <162672490+tylersummers@users.noreply.github.com>
2025-02-03 15:49:28 +0900

    Add sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    
+    // Return ERROR for non-positive integers
+    if (!(Number.isInteger(a) && a > 0) || !(Number.isInteger(b) && b > 0)) {
+        return "ERROR"
+    }
 
-};
+    // Push numbers into an array
+    let lowNum = Math.min(a, b);
+    let highNum = Math.max(a, b);
+    const array = [];
+    for (let i = lowNum; i <= highNum; i++) {
+        array.push(i)
+    }
+
+    // Sum each number in the array
+    let sum = 0;
+    array.forEach(num => {
+        sum += num;
+    })
+    return sum
+}
 
 // Do not edit below this line
 module.exports = sumAll;
