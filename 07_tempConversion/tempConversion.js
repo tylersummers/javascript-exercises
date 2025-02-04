a6a3d43086860c9792ba569c4c2687ec92d893c7
tylersummers <162672490+tylersummers@users.noreply.github.com>
2025-02-04 10:18:20 +0900

    Add functions to convert temperatures

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreesF) {
+  let degreesC = (degreesF - 32) * (5 / 9)
+  return Number(degreesC.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreesC) {
+  let degreesF = degreesC * (9 / 5) + 32
+  return Number(degreesF.toFixed(1))
 };
 
 // Do not edit below this line
