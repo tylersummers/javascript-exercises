e5fc551a74cf0d83b93c75595d2ef5dde0424cce
tylersummers <162672490+tylersummers@users.noreply.github.com>
2025-03-05 14:59:17 +0900

    Add function logic

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a , b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, current) => total + current, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product, current) => product * current)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b)
 };
 
 const factorial = function() {
-	
+	let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
